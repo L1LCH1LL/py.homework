6cbddd99c4f354951aa708518bcfa6f045cea659
L1LCH1LL <108305081+L1LCH1LL@users.noreply.github.com>
2025-03-04 18:22:12 +0300

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,7 @@
+a = 8
+b = 10
+c = 12
+d = 18
+ches = (-3+a**2)*b-2**3
+thnm = c-2*d
+print(ches/thnm)
---
+++ 1.2.py
@@ -0,0 +1,5 @@
+minutes = int(input('Введите количество минут: '))
+ch = minutes//60
+minn = minutes%60
+print('часов', ch)
+print('минут', minn)
